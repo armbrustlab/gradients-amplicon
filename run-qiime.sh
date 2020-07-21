80fdee0a32c105a0c10f52355b52b9dd4ebb24d4
Bennett Lambert <lambertb@uw.edu>
2020-07-21 13:04:59 -0700

    Update run-qiime.sh

--- run-qiime.sh
+++ run-qiime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# run-qiime.sh START_ARTIFACT EARLY_STOP
+# run-qiime.sh START_ARTIFACT QC PROC
 
 # processing pipeline for 18S analysis in qiime2
 # read each dataset into qiime2 artifact format using a
@@ -10,48 +10,32 @@ file=$1
 filename=$(basename -- "$file")
 filename_no_ext="${filename%.*}"
 
-es=$2
+qc=$2
+
+process=$3
 
 mkdir int_files
-mkdir run_stats
+mkdir int_files/viz
 mkdir output_tables
 
-# trim adapter sequences
-qiime cutadapt trim-single --i-demultiplexed-sequences "${file}" \
---p-cores 8 --p-front ^CAGCAGCCGCGGTAATTCC \
---p-discard-untrimmed \
---p-no-indels --p-error-rate 0.2 \
---o-trimmed-sequences "int_files/${filename_no_ext}-trimmed.qza"
+if [qc == true]
+then
+qiime cutadapt trim-single --i-demultiplexed-sequences "./int_files/${filename}"  --p-cores 8 \
+--p-adapter AGACCAAGTCTCTGC CAAGCAGAAGACGGCATACGAGAT CCCGTGTTGAGTCAAATTAAGC  --p-front CAGCAGCCGCGGTAATTCC ACACTGACGACATGGTTCTACA AATGATACGGCGACCACCGAGATCT \
+--p-discard-untrimmed  --p-no-indels --p-error-rate 0.2  --o-trimmed-sequences "./int_files/${filename_no_ext}-trimmed.qza"
 
-# visualize read quality following trimming.
-qiime demux summarize \
-  --i-data "int_files/${filename_no_ext}-trimmed.qza" \
-  --o-visualization "run_stats/${filename_no_ext}-demux.qzv"
+qiime demux summarize --i-data "./int_files/${filename_no_ext}-trimmed.qza" --o-visualization "int_files/viz/${filename_no_ext}-demux.qzv"
+fi
 
-# the first time we run this we want to stop to look at read quality, 
-# which we need for later.
-if [ es == true ]
-  then
-    exit(1)
+if [ process == true]
+then
+nohup qiime dada2 denoise-single --i-demultiplexed-seqs "./int_files/${filename_no_ext}-trimmed.qza" --p-trunc-len 200 --p-n-threads 8 \ 
+--o-representative-sequences "./int_files/${filename_no_ext}-rep-seqs.qza" \
+--o-table ./int_files/g1-18S-base-artifact-table.qza --o-denoising-stats "./int_files/${filename_no_ext}-denoising-stats.qza" &
 
-# begin the denoising process (this may take a while)
-qiime dada2 denoise-single \
-  --i-demultiplexed-seqs "int_files/${filename_no_ext}-trimmed.qza" \
-  --p-trunc-len 220 \
-  --p-n-threads 8 \
-  --o-representative-sequences "int_files/${filename_no_ext}-rep-seqs.qza" \
-  --o-table "int_files/${filename_no_ext}-table.qza" \
-  --o-denoising-stats "run_stats/${filename_no_ext}-stats.qza"
+qiime metadata tabulate --m-input-file "./int_files/${filename_no_ext}-denoising-stats.qza" --o-visualization "int_files/viz/${filename_no_ext}-denoising-stats.qzv"
 
-# create a visual for the denoising statistics
-qiime  metadata  tabulate \
---m-input-file "run_stats/${filename_no_ext}-stats.qza" \ 
---o-visualization "run_stats/${filename_no_ext}-stats.qzv"
-
-# run vsearch to get annotations
-qiime feature-classifier classify-consensus-vsearch \ 
---i-reference-reads db/pr2-mothur.qza 
---i-reference-taxonomy db/pr2-tax.qza \
---i-query "int_files/${filename_no_ext}-rep-seqs.qza" \ 
---p-threads 8 \
---o-classification "int_files/${filename_no_ext}-taxonomy-pr2.qza"
+nohup qiime feature-classifier classify-consensus-vsearch --i-reference-reads ../db/pr2/pr2-mothur.qza \ 
+--i-reference-taxonomy ../db/pr2/pr2-tax.qza --i-query "./int_files/${filename_no_ext}-rep-seqs.qza" --p-threads 8 \ 
+--o-classification "./int_files/${filename_no_ext}-taxonomy.qza" &
+fi
