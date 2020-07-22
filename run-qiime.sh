cd2c751efbea785561b6c70ff85cc6f9efb4d97e
Bennett Lambert <lambertb@uw.edu>
2020-07-22 12:55:20 -0700

    Update run-qiime.sh

--- run-qiime.sh
+++ run-qiime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# run-qiime.sh START_ARTIFACT QC PROC
+# run.sh START_ARTIFACT QC PROC
 
 # processing pipeline for 18S analysis in qiime2
 # read each dataset into qiime2 artifact format using a
@@ -10,11 +10,21 @@ file=$1
 filename=$(basename -- "$file")
 filename_no_ext="${filename%.*}"
 
+echo "filename with ext: ${filename}"
+echo "filename no ext: ${filename_no_ext}"
+
 qc=$2
+
+echo "qc = ${qc}"
+
 process=$3
 
-if [qc == true]
+echo "process = ${process}"
+
+if  [ $qc = true ]
 then
+echo "Beginning qc"
+
 qiime cutadapt trim-single --i-demultiplexed-sequences "./int_files/${filename}"  --p-cores 8 \
 --p-adapter AGACCAAGTCTCTGC CAAGCAGAAGACGGCATACGAGAT CCCGTGTTGAGTCAAATTAAGC  --p-front CAGCAGCCGCGGTAATTCC ACACTGACGACATGGTTCTACA AATGATACGGCGACCACCGAGATCT \
 --p-discard-untrimmed  --p-no-indels --p-error-rate 0.2  --o-trimmed-sequences "./int_files/${filename_no_ext}-trimmed.qza"
@@ -22,15 +32,15 @@ qiime cutadapt trim-single --i-demultiplexed-sequences "./int_files/${filename}"
 qiime demux summarize --i-data "./int_files/${filename_no_ext}-trimmed.qza" --o-visualization "int_files/viz/${filename_no_ext}-demux.qzv"
 fi
 
-if [ process == true]
+if [ $process = true ]
 then
-nohup qiime dada2 denoise-single --i-demultiplexed-seqs "./int_files/${filename_no_ext}-trimmed.qza" --p-trunc-len 200 --p-n-threads 8 \ 
+
+qiime dada2 denoise-single --i-demultiplexed-seqs "./int_files/${filename_no_ext}-trimmed.qza" --p-trunc-len 200 --p-n-threads 8 \ 
 --o-representative-sequences "./int_files/${filename_no_ext}-rep-seqs.qza" \
---o-table ./int_files/g1-18S-base-artifact-table.qza --o-denoising-stats "./int_files/${filename_no_ext}-denoising-stats.qza" &
+--o-table ./int_files/g1-18S-base-artifact-table.qza --o-denoising-stats "./int_files/${filename_no_ext}-denoising-stats.qza" 
 
 qiime metadata tabulate --m-input-file "./int_files/${filename_no_ext}-denoising-stats.qza" --o-visualization "int_files/viz/${filename_no_ext}-denoising-stats.qzv"
 
-nohup qiime feature-classifier classify-consensus-vsearch --i-reference-reads ../db/pr2/pr2-mothur.qza \ 
+qiime feature-classifier classify-consensus-vsearch --i-reference-reads ../db/pr2/pr2-mothur.qza \ 
 --i-reference-taxonomy ../db/pr2/pr2-tax.qza --i-query "./int_files/${filename_no_ext}-rep-seqs.qza" --p-threads 8 \ 
---o-classification "./int_files/${filename_no_ext}-taxonomy.qza" &
-fi
+--o-classification "./int_files/${filename_no_ext}-taxonomy.qza" 
